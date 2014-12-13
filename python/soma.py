ab925e02641271c17a88e282c6ed0be34dfcb955
Maurizio Crocci <mbcrocci@gmail.com>
2014-12-13 15:31:39 +0000

    Update soma.py

--- python/soma.py
+++ python/soma.py
@@ -1,17 +1,9 @@
 from sys import argv
 
-
-def soma(start, end):
-    soma = 0
-    for i in range(start, end+1):
-        soma += i
-
-    return soma
-
 if len(argv) < 2:
     print("Este programa soma todos os numeros desde um 'start' ate um 'end'")
     start = int(input("Introduza o 'start': "))
     end = int(input("Introduza o 'end': "))
 
 print("A soma de todos os numeros de %d a %d e: %d"
-      % (start, end, soma(start, end)))
+      % (start, end, sum(range(start, end+1)))
